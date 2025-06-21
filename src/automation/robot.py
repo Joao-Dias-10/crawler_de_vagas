77382b46b48923ca783c979bbb955457e46ee27a
João Dias <98277523+Joao-Dias-10@users.noreply.github.com>
2025-06-20 22:56:30 -0300

    Ajustes e otimizações nos módulos
    - Remoção de tratativas desnecessárias em limpeza.py
    - Reprocessamento nas queries do queries.py para evitar duplicação de dados
    - Finalização do fluxo de coleta de vagas em robot.py, ajustando os parâmetros de coleta do Selenium para cada variável

--- src/automation/robot.py
+++ src/automation/robot.py
@@ -2,46 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 import os
+import time
+import re
 
 from src.utils.configsAll import *
 
 
-
 def coletar_vagas(palavra_chave):
-
     driver = webdriver.Chrome()
-    driver.get(f"https://www.linkedin.com/login/pt")
+    wait = WebDriverWait(driver, 30)
+    driver.get("https://www.linkedin.com/login/pt")
     driver.maximize_window()
 
-    user = WebDriverWait(driver, 1000).until(EC.visibility_of_element_located((By.NAME, "session_key")))
+    user = wait.until(EC.visibility_of_element_located((By.NAME, "session_key")))
     user.send_keys(os.getenv('USER_LINKEDIN'))
-    password = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "session_password")))
+
+    password = wait.until(EC.visibility_of_element_located((By.NAME, "session_password")))
     password.send_keys(os.getenv('PASSWORD_LINKEDIN'))
     password.send_keys(Keys.ENTER)
 
-    nomeUserVlidacaoDeAcesso = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.CLASS_NAME, "profile-card-member-details")))
+    # Validação de login
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "profile-card-member-details")))
 
     driver.get(f"https://www.linkedin.com/jobs/search/?keywords={palavra_chave.replace(' ', '%20')}")
+    time.sleep(5)  
+
     vagas = []
-    elementos = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[7]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul')))
-    elementos = driver.find_elements(By.XPATH, '/html/body/div[7]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul')[:5]
-    for el in elementos:
-        try:
-            titulo = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            empresa = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            localizacao = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            url = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
+
+    # Esperar a lista de vagas carregar
+    ul_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'XLsfDgRPWWqswmzbPfSkbwlIWKwwAGMMWV')))
+
+    for i in range(1,6):
+            # Procurar elementos que contenham as classes esperadas (mesmo que parcialmente)
+            titulo = wait.until(EC.presence_of_element_located((By.XPATH, rf"/html/body/div[7]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul/li[{i}]/div/div/div[1]/div[1]/div[2]/div[1]/a/span[1]/strong"))).text
+            empresa = wait.until(EC.presence_of_element_located((By.XPATH, rf"/html/body/div[7]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul/li[{i}]/div/div/div[1]/div[1]/div[2]/div[2]/span"))).text
+            localizacao = wait.until(EC.presence_of_element_located((By.XPATH, rf"/html/body/div[7]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul/li[{i}]/div/div/div[1]/div[1]/div[2]/div[3]/ul/li/span"))).text
+            url = wait.until(EC.presence_of_element_located((By.XPATH, rf"/html/body/div[7]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul/li[{i}]/div/div/div[1]/div[1]/div[2]/div[1]/a"))).get_attribute("href")
+
             vagas.append({
-                "titulo": titulo,
+                "titulo": titulo,   
                 "empresa": empresa,
                 "localizacao": localizacao,
                 "url": url
             })
-        except:
-            continue
 
     driver.quit()
     return vagas
--- src/db/queries.py
+++ src/db/queries.py
@@ -1,11 +1,15 @@
 from src.db.models import VagaEmprego
 from sqlalchemy.orm import Session
 
-def vaga_existe(db: Session, url: str) -> bool:
-    return db.query(VagaEmprego).filter(VagaEmprego.url == url).first() is not None
+def vaga_existe(db: Session, titulo: str, empresa: str, localizacao: str) -> bool:
+    return db.query(VagaEmprego).filter(
+        VagaEmprego.titulo == titulo,
+        VagaEmprego.empresa == empresa,
+        VagaEmprego.localizacao == localizacao
+    ).first() is not None
 
 def inserir_vagas_no_banco(db: Session, vaga: dict):
-    if not vaga_existe(db, vaga["url"]):
+    if not vaga_existe(db, vaga["titulo"], vaga["empresa"], vaga["localizacao"]):  
         nova_vaga = VagaEmprego(
             titulo=vaga["titulo"],
             empresa=vaga["empresa"],
